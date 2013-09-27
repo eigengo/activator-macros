22b5b9801e18cdd097d170665f64700eb4d0f86a
Jan Machacek <jan.machacek@gmail.com>
2013-09-27 17:38:09 +0100

    Macro paradise in prep for 2.11 macros

--- project/build.scala
+++ project/build.scala
@@ -37,9 +37,11 @@ object BuildSettings {
     javacOptions in Compile ++= Seq("-source", "1.6", "-target", "1.7", "-Xlint:unchecked", "-Xlint:deprecation", "-Xlint:-options"),
     javaOptions += "-Djava.util.logging.config.file=logging.properties",
     javaOptions += "-Xmx2G",
+    //scalaVersion := "2.11.0-M5",
     outputStrategy := Some(StdoutOutput),
     fork := true,
     maxErrors := 1,
+    addCompilerPlugin("org.scala-lang.plugins" % "macro-paradise" % "2.0.0-SNAPSHOT" cross CrossVersion.full),
     resolvers ++= Seq(
       Resolver.mavenLocal,
       Resolver.sonatypeRepo("releases"),
